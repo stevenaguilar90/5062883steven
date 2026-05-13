65a9ceeb9619867ae4cd4ff183a4015cf6ad0ac4
stevenaguilar90 <5062883@clases.edu.sv>
2026-05-13 16:04:18 -0600

    Add files via upload

---
+++ division.py
@@ -0,0 +1,4 @@
+n1= 23
+n2= 15
+division= n1 / n2
+print("el resultado de la division es:", division)
---
+++ multiplicacion.py
@@ -0,0 +1,4 @@
+n1= 23
+n2= 15
+multiplicacion= n1 * n2
+print("el resultado de la multiplicacion es:", multiplicacion)
